eb83729bc427522bce9c1554ae2595cfa605d758
Alex Clayton <alexander.clayton.email@gmail.com>
2023-04-12 10:43:50 -0500

    heroku

--- server/server.js
+++ server/server.js
@@ -30,12 +30,12 @@ app.use(express.json());
 //     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 // });
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(express.static(path.join(__dirname, '../client/public')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
 };
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 const startApolloServer = async () => {
