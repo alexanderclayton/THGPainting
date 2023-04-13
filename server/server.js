00b9e2cb98a1e26ab88cc60916703324f91fa278
Alex Clayton <alexander.clayton.email@gmail.com>
2023-04-13 11:56:31 -0500

    heroku attempt 11

--- server/server.js
+++ server/server.js
@@ -10,14 +10,16 @@ import typeDefs from './schemas/typeDefs.js';
 import resolvers from './schemas/resolvers.js'
 import db from './config/connection.js';
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: { authMiddleware, clientMiddleware, }
-  });
+  typeDefs,
+  resolvers,
+  context: { authMiddleware, clientMiddleware, }
+});
+
+console.log( "dirname:", __dirname);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -31,22 +33,23 @@ app.use(express.json());
 // });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-    });
-  }
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+});
 
 const startApolloServer = async () => {
-    await server.start();
-    server.applyMiddleware({ app });
+  await server.start();
+  server.applyMiddleware({ app });
 
-    db.once('open', () => {
-        app.listen(PORT, () => {
-            console.log(`API server running on port ${PORT}`)
-            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-        });
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}`)
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
+  });
 };
 
 startApolloServer();
